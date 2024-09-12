469354b7d774efa61f1b46b6fc223b3b8aa840bf
Yulianny <yuliannydev@gmail.com>
2024-09-12 18:14:30 -0400

    fix bug

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel/static';
+//import vercel from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [tailwind()],
-	output: 'server',
-	adapter: vercel(),
+	//	output: 'server',
+	//	adapter: vercel(),
 });
