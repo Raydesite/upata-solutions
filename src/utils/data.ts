1de92bae6d8c4e5dec75700949c9d9f7b59cb763
Yulianny <yuliannydev@gmail.com>
2024-09-15 17:52:40 -0400

    Titles of services, and content without icons.

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,46 +1,41 @@
 const services = [
 	{
 		title: 'Restoration',
-		description:
-			'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti qui soluta cupiditate',
+		description: 'Tiles, Kitchen, Bathrooms.',
 		icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
         </svg>`,
 	},
 	{
-		title: 'Installation',
-		description:
-			'Sunt, ipsam, necessitatibus sint fugit officia laboriosam minima ab ullam at magni et. Quaerat, sint!',
+		title: 'Carpentry',
+		description: 'Closets, Doors, Moulding.',
 		icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
         </svg>`,
 	},
 	{
 		title: 'Paint',
-		description:
-			'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti qui soluta cupiditate',
+		description: 'Houses, Doors, Cabinets, Bathtubs.',
 		icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
         </svg>`,
 	},
 	{
 		title: 'Decorations',
-		description:
-			'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti qui soluta cupiditate',
+		description: 'Wall design, Interior design, Exterior design.',
 		icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
         </svg>`,
 	},
 	{
-		title: 'Services 5',
-		description:
-			'Sunt, ipsam, necessitatibus sint fugit officia laboriosam minima ab ullam at magni et. Quaerat, sint!',
+		title: 'Electricity',
+		description: 'Cabling, Lamps, Fans, and general electrical services.',
 		icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
         </svg>`,
 	},
-	{
+	/** {
 		title: 'Services 6',
 		description:
 			'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti qui soluta cupiditate',
 		icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
         </svg>`,
-	},
+	}, */
 ];
 
 export { services };
